511004e60e52df6d88ef27ebef740ded9572616e
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-03 01:19:35 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout';
 
 const IndexPage = () => {
   
   return (
-  <div>
-    <Header/>
-    <h1>Hello.</h1>
-    <p>These are the recipes I have collected from my mum and others over the years.  My book is falling apart with loose leaf recipes falling out.  I'm always sharing recipes, so now I can just direct to this site instead of taking </p>
-    <Footer/>
-  </div>
+    <Layout>
+      <h1>Hello.</h1>
+      <p>These are the recipes I have collected from my mum and others over the years.  My book is falling apart with loose leaf recipes falling out.  I'm always sharing recipes, so now I can just direct to this site instead of taking </p>
+    </Layout>
   )
 }
 
