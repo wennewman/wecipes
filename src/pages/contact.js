eb15c0c25a6be75e9ada4a0cbd82c41af0a69d49
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-03 01:47:06 +1000

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout';
 
 const ContactPage = () => {
 
     return (
     <div>
-        <Header/>
         <h1>Contact me</h1>
         <p>If you would like to add a recipe or provide some feedback, hit me up on <a href="https://nitter.net/nuwms">@nuwms on twitter.</a></p>
-        <Footer/>
     </div>
     )
 }
