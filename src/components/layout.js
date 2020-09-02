6eeea4673c63cd863f523957dea7111c65504948
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-03 01:16:59 +1000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-const Lyaout = () => {
+const Layout = () => {
   return (
     <div>
       <header/>
-      
+        
       <footer/>
     </div>
   )
 }
+export default Layout
