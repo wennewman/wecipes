a59b76c79bc006fd5622e3b56516c0c03b2d5902
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-03 01:45:47 +1000

    Update recipe.js

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout';
 
 const RecipePage = () => {
 
     return (
     <div>
-        <Header/>
         <h1>Recipe</h1>
         <p>Recipes will show up here later on</p>
-        <Footer/>
     </div>
     )
     
