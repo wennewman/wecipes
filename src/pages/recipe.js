0ee96f9f1b321af2c0f30b42e461a68e88d61348
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-03 01:49:24 +1000

    Update recipe.js

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -4,10 +4,10 @@ import Layout from '../components/layout';
 const RecipePage = () => {
 
     return (
-    <div>
+    <Layout>
         <h1>Recipe</h1>
         <p>Recipes will show up here later on</p>
-    </div>
+    </Layout>
     )
     
 }
