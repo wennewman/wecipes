523bfc36ae8b7f2d099612ec59ce3ecb4a537aa8
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-07 04:16:00 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 import Layout from '../components/layout';
-import Head from '../components/head;
+import Head from '../components/head';
 
 const IndexPage = () => {
   
   return (
     <Layout>
-      <Head title="Home" />
+      <Head title="Home"/>
       <h1>Hello.</h1>
       <p>Some profound words go here to describe the point of this website.  It's just recipes, not rocket science.  Some are not verified, just taken from a book from Mum</p>
     </Layout>
