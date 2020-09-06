6822406c5151881da54ee77c531718d7cbd96fb8
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-07 04:11:37 +1000

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby'
+import { Link } from 'gatsby';
 
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import Head from '../components/head';
 
 const NotFound = () => {
 
   return (
     <Layout>
+     <Head title="Page not found"/>
       <h1>Ooof! The page you are after insn't here</h1>
       <p>Where it has gone, is not quite clear</p>
       <p><Link to="/">Here is a link to get you back home</Link></p>
