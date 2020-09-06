2170f7e5c1482c8f855195924da187c5e2e5e651
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-07 02:26:49 +1000

    Update recipe.js

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const Recipe = () => {
-    return {
-        <Layout>
-        </Layout>
+export const query = graphql`
+    query ($slug: String!) {
+        markdownRemark (
+            fields: { slug: { eq: $slug } }) {
+        frontmatter {
+            title
+            date(formatString: "Do, MMMM, YYYY")
+            categories
+            author
+            description
+        }
+        html
+       }
+    }
+`
+const Recipe = (props) => {
+   return (
+       <Layout>
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <p>{props.data.markdownRemark.frontmatter.categories}</p>
+            <p>{props.data.markdownRemark.frontmatter.author}</p>
+            <div dangerouslySetInnerHTML={{___html: props.data.markdownRemark.frontmatter.html}}</div>
+       </Layout>
+   )
 }
-
 export default Recipe
