f128af42285ff58f899b7239bcc24611c3dc1395
wennewman <47679365+wennewman@users.noreply.github.com>
2020-09-07 04:09:11 +1000

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Head from '../components/head;
 
 const ContactPage = () => {
 
     return (
     <Layout>
+        <Head title="Contact"/>
         <h1>Contact</h1>
         <p>If you would like to add a recipe or provide some feedback, hit me up on something like twitter <a href="https://nitter.net/nuwms" target="_blank" rel="noreferrer" >@nuwms</a>.</p>
     </Layout>
