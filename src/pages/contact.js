e97be737b185d6b6431308759ee020ebf6737fdb
Nuwms <wennewman@gmail.com>
2020-09-08 22:01:59 +1000

    asdfasfd

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
     <Layout>
         <Head title="Contact"/>
         <h1>Contact.</h1>
-        <p>If you would like to add a recipe or provide some feedback, hit me up on something like nitter <a href="https://nitter.net/nuwms" target="_blank" rel="noreferrer" >@nuwms</a>.  Sad thing is, I never check nitter, so I probably won't respond.  You know who I am and how to find me:).</p>
+        <p>If you would like to add a recipe or provide some feedback, hit me up on something like Nitter <a href="https://nitter.net/nuwms" target="_blank" rel="noreferrer" >@nuwms</a>.  Sad thing is, I never check nitter, so I probably won't respond.  You know who I am and how to find me:).</p>
     </Layout>
     )
 }
