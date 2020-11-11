2f1b6823cd8b73458489e17b8e469c215c581b25
Nuwms <wennewman@gmail.com>
2020-11-11 12:33:58 +1100

    mid lint config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,38 +7,62 @@
 module.exports = {
   siteMetadata: {
     title: 'No more scrap paper...',
-    author: 'Ed Winchester test kitchen'
+    author: 'Ed Winchester test kitchen',
   },
 
   plugins: [
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
+      options: {
+        exclude: /(node_modules|.cache|public)/,
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            '**/*.{css,scss,less}',
+            '**/*.{json,json5}',
+            '**/*.{graphql}',
+            '**/*.{md,mdx}',
+            '**/*.{html}',
+            '**/*.{yaml,yml}',
+          ],
+        },
+        eslint: {
+          patterns: '**/*.{js,jsx,ts,tsx}',
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    
+
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
         path: `${__dirname}/src/`,
-      }
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
         path: `${__dirname}/src/images/`,
-      }
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'fonts',
         path: `${__dirname}/src/fonts/`,
-      }
+      },
     },
-    
+
     'gatsby-plugin-sharp',
     `gatsby-transformer-sharp`,
-    
+
     {
       resolve: 'gatsby-transformer-remark',
       options: {
@@ -48,11 +72,11 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 750,
-              linkImagesToOriginal: false
-            }
-          }
-        ]
-      }
-    }
-  ]
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
---
+++ src/recipes/sausageRolls/sausageRolls.md
@@ -0,0 +1,30 @@
+---
+title: "Sausage rolls"
+date: "2020-11-11"
+categories: "dinner"
+credit: "Me"
+description: "I generally wing it when making these.  Also not something I make very often but had a recipe request for a recent batch, so here tis.  Some like to add a tablespoon of relish to the meat mix; this adds some sweet.  Up to you.  Make them as big or small as you like"
+---
+
+###Ingredients
+Home made rough puff or store bought puff pastry  
+500g sausage mince  
+500g lean beef mince  
+2 large grated carrots  
+1 large finely chopped onion  
+¼ cup finely chopped fresh parsley  
+1 tsp salt  
+ground black pepper to your taste (I usually put almost 1 tbsp)  
+1 tsp garlic powder  
+1 tsp paprika  
+
+###Method
+1. Turn on your oven to 220ºc
+2. Take the pastry sheets out of the freezer before you start if using frozen pastry
+3. Combine all other ingredients in a large bowl (I usually use wet hands to do this)
+4. Lay out your pastry and make about a 3cm sausage out of the meat mix and lay along the edge of the pastry.  I usually do one, then use it as a guide for the rest
+5. Repeat until you have run out of meat and/or pastry (either leftover can go back in the freezer)
+6. Before cutting them, brush with a lightly beaten egg or some milk.  You can sprinkle with sesame or nigella seeds if you like
+7. Cut them to whatever size you like
+8. Cooking time will vary depending the size you make them.  Start checking about 20 minutes in for a golden brown top and no juice running out of the middle of the meat
+
